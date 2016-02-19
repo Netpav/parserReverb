2ba3d0b948a5e06e72c003cdf7bd163827983d0b
Jonas Petrovsky <jond@post.cz>
2016-02-19 16:31:45 +0100

    Command line interface changed to sys.argv, so it can be used on all (even old) systems. Module argparse is present only in python >= 2.7

--- run_reverb.py
+++ run_reverb.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from src.ReverbParser import ReverbParser
 
@@ -7,7 +8,7 @@ current_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Set filepaths
 reverb_filepath = os.path.abspath(current_dir+'/libs/reverb-latest.jar')
-output_dir = current_dir
+# output_dir = current_dir
 
 # Create the main object.
 reverb_parser = ReverbParser(reverb_filepath)
@@ -19,17 +20,23 @@ reverb_parser = ReverbParser(reverb_filepath)
 
 # If the file was run directly (as script).
 if __name__ == '__main__':
-    # Module required for command line parsing.
-    import argparse
+    # # Module required for command line parsing.
+    # import argparse
+    #
+    # # Parse the incoming arguments: ./run_reverb.py <input filepath> <output directory>
+    # args_parser = argparse.ArgumentParser(description='Reverb caller and parser in Python')
+    # args_parser.add_argument('infile', action='store', help='Path to the input file.',
+    #                     metavar='<string>', nargs=1)
+    # args_parser.add_argument('outdir', action='store', help='Path to the output directory.',
+    #                     metavar='<string>', nargs=1)
+    # args = args_parser.parse_args()
+    # reverb_parser.process_file(args.infile[0], args.outdir[0])
 
-    # Parse the incoming arguments: ./run_reverb.py <input filepath> <output directory>
-    args_parser = argparse.ArgumentParser(description='Reverb caller and parser in Python')
-    args_parser.add_argument('infile', action='store', help='Path to the input file.',
-                        metavar='<string>', nargs=1)
-    args_parser.add_argument('outdir', action='store', help='Path to the output directory.',
-                        metavar='<string>', nargs=1)
-    args = args_parser.parse_args()
+    # Simple solution for all (even old) systems.
+    in_file = sys.argv[1]
+    out_dir = sys.argv[2]
 
-    # Process arguments - if it's relative path.
-    reverb_parser.process_file(args.infile[0], args.outdir[0])
+    reverb_parser.process_file(in_file, out_dir)
+
+
 
